69387055ba66812f9463e10fa1a7018925d9a75a
Himanshu25a <anshuhimanshu787@gmail.com>
2026-01-14 10:53:13 +0530

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -3,6 +3,8 @@
 # for i in range(0,len(name)):
 #     print(name)
 
-n=int(input("Enter the number"))
-for i in range(0,n+1,11):
-    print(i)
+n=int(input("Enter the number:"))
+for i in range(1,11):
+    print(n,"x",i ,"=", n*i )
+
+    
