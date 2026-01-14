43f145e2f6e38fb583bbd18882e2d99081ced447
Himanshu25a <anshuhimanshu787@gmail.com>
2026-01-14 11:58:31 +0530

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -3,8 +3,68 @@
 # for i in range(0,len(name)):
 #     print(name)
 
-n=int(input("Enter the number:"))
-for i in range(1,11):
-    print(n,"x",i ,"=", n*i )
+# n=int(input("Enter the number:"))
+# for i in range(1,11):
+#     print(n,"x",i ,"=", n*i )
+
+# n=int(input("Enter the Number: "))
+# print(n**0.5) 
+# # ** means square root
+# if n<=1:
+#     print("False")
+# else:
+#     prime=True
+#     for i in range(2,n):
+#         if n%i==0:
+#             prime=False
+#     print(prime)
+
+# n=int(input("Enter the Number: "))
+# print(n**0.5) 
+# # ** means square root
+# if n<=1:
+#     print("False")
+# else:
+#     prime=True
+#     for i in range(2,int(n**0.5)+1):
+#         if n%i==0:
+#             prime=False
+#     print(prime)
 
     
+
+# n=int(input("Enter The Number:"))
+
+
+
+
+
+
+
+
+
+
+
+# n=(int(input("Enter the Number:")))
+# for i in range(0,101):
+#     print (i) 
+#     if i%3==0:
+#         print("Fizz")
+#     elif i%5==0:
+#         print("Buzz")
+#     elif i%3 and i%5 ==0:
+#         print("Fizz Buzz")
+#     else:
+#         print("Not Divisible")
+
+# n=(int(input("Enter the Number:")))
+# # for i in range(0,101):
+# #     print (i) 
+# if n%3==0:
+#        print("Fizz")
+# elif n%5==0:
+#        print("Buzz")
+# elif n%3 and n%5 ==0:
+#        print("Fizz Buzz")
+# else:
+#       print("Not Divisible")
