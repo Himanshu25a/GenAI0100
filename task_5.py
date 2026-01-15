f3d6cdb73eee547c743427327301542d189f4efa
Himanshu25a <anshuhimanshu787@gmail.com>
2026-01-15 12:08:32 +0530

    Update task_5.py

---
+++ task_5.py
@@ -0,0 +1,10 @@
+# str=input("Enter Your string:")
+# if (str.startswith("co")) and (str.endswith("ts")):
+#     print("cots")
+# elif (str.startswith("ne")) and (str.endswith("ar")):
+#     print("near")
+# elif (len(str))<2:
+#     print("Not a valid String")
+# else:
+#     print("Not a valid String")
+
